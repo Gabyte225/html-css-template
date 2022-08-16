8198547f6d624bd0b77215e09209dbb72a46b2e4
Gabyte225 <gabe300@gmail.com>
2022-08-16 21:24:13 +0300

    changes

--- src/localStorage.js
+++ src/localStorage.js
@@ -4,10 +4,34 @@ const phone = document.querySelector('.phone');
 const address = document.querySelector('.address');
 const services = document.querySelector('.services');
 
-let content = document.querySelector(".result");
+// let content = document.querySelector(".result");
 
 document.querySelector(".Submit").addEventListener("click", () => {
-  content.innerHTML = `${fullName.value} </br> ${email.value} </br> ${phone.value} </br> ${address.value} </br> ${services.value}`;
+  let newName = document.createElement('div');
+  document.querySelector(".result").appendChild(newName);
+  newName.className = 'fullName';
+  document.querySelector('.fullName').textContent = `${fullName.value}`;
+
+  let newEmail = document.createElement('div');
+  document.querySelector(".result").appendChild(newEmail);
+  newEmail.className = 'newEmail';
+  document.querySelector('.newEmail').textContent = `${email.value}`;
+
+  let newPhone = document.createElement('div');
+  document.querySelector(".result").appendChild(newPhone);
+  newPhone.className = 'newPhone';
+  document.querySelector('.newPhone').textContent = `${phone.value}`;
+
+  let newAddress = document.createElement('div');
+  document.querySelector(".result").appendChild(newAddress);
+  newAddress.className = 'newAddress';
+  document.querySelector('.newAddress').textContent = `${address.value}`;
+
+  let newServices = document.createElement('div');
+  document.querySelector(".result").appendChild(newServices);
+  newServices.className = 'newServices';
+  document.querySelector('.newServices').textContent = `${services.value}`;
+
   localStorage.setItem('fullName', fullName.value);
   localStorage.setItem('email', email.value);
   localStorage.setItem('phone', phone.value);
